40fbc4c9ee097ad15a9261f81de5140c4e0d3188
Lorenzo Padoan <lorenzo.padoan977@gmail.com>
2023-12-17 17:14:30 +0000

    FIX dateset simulation

--- dataset_generation/data_simulation.py
+++ dataset_generation/data_simulation.py
@@ -12,7 +12,7 @@ number_of_people = 1000
 group_of_people = ['high_school_student', 'college_student', 'adult', 'elderly']
 
 motifs = {
-        'high_school_student': ['home', 'school', 'sport_centre','home'],
+        'high_school_student': ['home', 'school', 'home','sport_centre','home'],
         'college_student': ['home', 'university', 'cafe', 'library', 'pub','home'],
         'adult': ['home', 'office', 'gym', 'supermarket','home'],
         'elderly': ['home', 'bar', 'supermarket', 'pharmacy','home']
@@ -34,7 +34,7 @@ for i in range(0, number_of_people):
 
 #############################################################
 # assign home location to each persone using the residential area given by the shapefile
-landuse_gdf = gpd.read_file('./shp_padova/landuse.shp')
+landuse_gdf = gpd.read_file('dataset_generation/shp_padova/landuse.shp')
 landuse_gdf = landuse_gdf.to_crs(epsg=4326)
 
 # finf the areas that have column type = residential
@@ -50,7 +50,7 @@ for i in range(0, number_of_people):
 
 
 # collect all the pois from buildings shapefile
-buildings_gdf = gpd.read_file('shp_padova/buildings.shp')
+buildings_gdf = gpd.read_file('dataset_generation/shp_padova/buildings.shp')
 buildings_gdf = buildings_gdf.to_crs(epsg=4326)
 pois = buildings_gdf
 
@@ -111,69 +111,78 @@ start_date = datetime.datetime.strptime('2023-02-06', '%Y-%m-%d')
 end_date = datetime.datetime.strptime('2023-02-10', '%Y-%m-%d')
 delta = datetime.timedelta(days=1)
 
+lst_days = []
+while start_date <= end_date:
+    lst_days.append(start_date)
+    start_date += delta
+
 # Create an empty dataframe to store the origin and destination data
 df = pd.DataFrame(columns=['Date','User', 'origin_timestamp', 'destination_timestamp', 'origin_coordinates', 'destination_coordinates','path'])
 
-while start_date <= end_date:
-    print(start_date.strftime('%Y-%m-%d'))
-    date = start_date.strftime('%Y-%m-%d')
-    unix_start_time = int(start_date.replace(hour=0).timestamp())
-    for person_id, person in tqdm(people.items()):
-
-        point_a = person['home_location']
-        unix_start_time_temp = int(start_date.replace(hour=7).timestamp())
-        for motif in motifs[person['group']]:
-            gps_trace = []
-
-            # randomly perturbate the timestamp of the day of 7 am
-            unix_start_time = np.random.randint(unix_start_time_temp - 3600, unix_start_time_temp + 3600)
-            if motif != 'home':
-                # get the pois of that type
-                pois_of_that_type = pois_dict[motif]
-                # choose the nearest poi of that type
-                nearest_poi = get_nearest_poi(point_a, pois_of_that_type).representative_point()
-                # generate gps trace from home to the nearest poi
-                gps_trace.extend(generate_gps_trace_from_to(point_a, nearest_poi, unix_start_time))
-                origin = point_a
-                point_a = nearest_poi
-
-                unix_time_arrival = gps_trace[-1][2]
-
-                # add the row to the dataframe
-                df = df._append({'Date': date, 'User': person_id,  'origin_timestamp': unix_start_time, 'destination_timestamp': unix_time_arrival, 'origin_coordinates': point_a, 'destination_coordinates': nearest_poi, 'path': gps_trace}, ignore_index=True)
-
-                # for each motif add an appropriate time interval of activity in the poi
-                if motif == 'school':
-                    unix_start_time += 3600 * np.random.randint(5, 7)
-                elif motif == 'university':
-                    unix_start_time += 3600 * np.random.randint(2, 6)
-                elif motif == 'cafe':
-                    unix_start_time += 3600 * np.random.randint(1, 2)
-                elif motif == 'library':
-                    unix_start_time += 3600 * np.random.randint(2, 4)
-                elif motif == 'pub':
-                    unix_start_time += 3600 * np.random.randint(1, 3)
-                elif motif == 'office':
-                    unix_start_time += 3600 * np.random.randint(6, 10)
-                elif motif == 'gym':
-                    unix_start_time += 3600 * np.random.randint(1, 2)
-                elif motif == 'supermarket':
-                    unix_start_time += 3600 * 1
-                elif motif == 'bar':
-                    unix_start_time += 3600 * np.random.randint(1, 2)
-                elif motif == 'pharmacy':
-                    unix_start_time += 3600 * 0.5
-                elif motif == 'sport_centre':
-                    unix_start_time += 3600 * np.random.randint(1, 2)
-                
+# generate Origin and Destination trajectoris for each person and for each day
+for day in tqdm(lst_days):
+    unix_start_time_temp = int(start_date.replace(hour=7).timestamp())
+    for i in range(0, number_of_people):
+        unix_start_time = np.random.randint(unix_start_time_temp - 3600, unix_start_time_temp + 3600)
+        # get the motif for the group of people of the person
+        motif = motifs[people[i]['group']]
+        # get the home location of the person
+        home_location = people[i]['home_location']
+        # iterate over all couples of movement in the motif
+        for j in range(len(motif) - 1):
+            if motif[j] == 'home':
+                origin = home_location
             else:
-                # append the home location to the gps trace
-                gps_trace.append((point_a.x, point_a.y, unix_start_time))
+                origin = get_nearest_poi(home_location, pois_dict[motif[j]])
+                origin = origin.representative_point()
+            
+            if motif[j + 1] == 'home':
+                destination = home_location
+            else:
+                destination = get_nearest_poi(home_location, pois_dict[motif[j + 1]])
+                destination = destination.representative_point()
+            
+            # generate the gps trace
+            gps_trace = generate_gps_trace_from_to(origin, destination, unix_start_time)
 
-                # add the row to the dataframe
-                df = df._append({'Date': date, 'User': person_id, 'origin_timestamp': unix_start_time, 'destination_timestamp': unix_start_time, 'origin_coordinates': point_a, 'destination_coordinates': person['home_location'], 'path': gps_trace}, ignore_index=True)
+            # add the gps trace to the dataframe
+            df = df._append({
+                'Date': day,
+                'User': i,
+                'origin_timestamp': gps_trace[0][2],
+                'destination_timestamp': gps_trace[-1][2],
+                'origin_coordinates': origin,
+                'destination_coordinates': destination,
+                'path': gps_trace
+            }, ignore_index=True)
 
-    start_date += delta
-    unix_start_time = int(datetime.datetime.strptime(date, '%Y-%m-%d').timestamp())
+            # update the unix_start_time
+            if motif[j + 1] == 'home':
+                unix_start_time += 3600 * np.random.randint(2, 4)
+            elif motif[j + 1] == 'school':
+                unix_start_time += 3600 * np.random.randint(5, 7)
+            elif motif[j + 1] == 'university':
+                unix_start_time += 3600 * np.random.randint(2, 6)
+            elif motif[j + 1] == 'cafe':
+                unix_start_time += 3600 * np.random.randint(1, 2)
+            elif motif[j + 1] == 'library':
+                unix_start_time += 3600 * np.random.randint(2, 4)
+            elif motif[j + 1] == 'pub':
+                unix_start_time += 3600 * np.random.randint(1, 3)
+            elif motif[j + 1] == 'office':
+                unix_start_time += 3600 * np.random.randint(6, 10)
+            elif motif[j + 1] == 'gym':
+                unix_start_time += 3600 * np.random.randint(1, 2)
+            elif motif[j + 1] == 'supermarket':
+                unix_start_time += 3600 * 1
+            elif motif[j + 1] == 'bar':
+                unix_start_time += 3600 * np.random.randint(1, 2)
+            elif motif[j + 1] == 'pharmacy':
+                unix_start_time += 3600 * 0.5
+            elif motif[j + 1] == 'sport_centre':
+                unix_start_time += 3600 * np.random.randint(1, 2)
+# conver origin column to string
+df['origin_coordinates'] = df['origin_coordinates'].astype(str)
+df['destination_coordinates'] = df['destination_coordinates'].astype(str)
 
-df.to_csv('gps_traces.csv', index=False)
+df.to_parquet('./gps_traces.parquet')
